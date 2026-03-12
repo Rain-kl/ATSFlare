b05bd608f259ed89444c1cd3279338955e98b17b
ryan <ryan@arctel.net>
2026-03-13 00:02:10 +0800

    [优化] 导航优化

--- atsf_server/web/lib/constants/navigation.ts
+++ atsf_server/web/lib/constants/navigation.ts
@@ -1,44 +1,46 @@
-import type { NavigationItem } from '@/types/navigation';
+import type {NavigationItem} from '@/types/navigation';
 
 export const dashboardNavigation: NavigationItem[] = [
-  {
-    href: '/',
-    label: '总览',
-    icon: 'home',
-  },
-  {
-    href: '/node',
-    label: '节点',
-    icon: 'node',
-  },
-  {
-    href: '/proxy-route',
-    label: '规则',
-    icon: 'proxy',
-  },
-  {
-    href: '/config-version',
-    label: '发布',
-    icon: 'release',
-  },
-  {
-    href: '/website',
-    label: '网站',
-    icon: 'website',
-  },
-  {
-    href: '/user',
-    label: '用户',
-    icon: 'user',
-  },
-  {
-    href: '/performance',
-    label: '性能',
-    icon: 'performance',
-  },
-  {
-    href: '/setting',
-    label: '设置',
-    icon: 'setting',
-  },
+    {
+        href: '/',
+        label: '总览',
+        icon: 'home',
+    },
+    {
+        href: '/node',
+        label: '节点',
+        icon: 'node',
+    },
+    {
+        href: '/website',
+        label: '网站',
+        icon: 'website',
+    },
+    {
+        href: '/proxy-route',
+        label: '规则',
+        icon: 'proxy',
+    },
+    {
+        href: '/config-version',
+        label: '发布',
+        icon: 'release',
+    },
+
+    {
+        href: '/performance',
+        label: '性能',
+        icon: 'performance',
+    },
+    {
+        href: '/user',
+        label: '用户',
+        icon: 'user',
+    },
+
+    {
+        href: '/setting',
+        label: '设置',
+        icon: 'setting',
+    },
 ];
